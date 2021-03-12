303fb591fbdc760defc8ed2ed26ed471cbc0e7c8
choiyoungmin <dudals3844@gmail.com>
2021-03-12 13:01:43 +0900

    Modify setup

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.0.1',
     url='https://github.com/dudals3844/quant_service',
     author='ChoiYoungMin',
-    auther_email='dudals3844@gmail.com',
+    author_email='dudals3844@gmail.com',
     description='Quant Service',
     packages=find_packages(exclude=['test']),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=['cython'],
+    install_requires=['pandas'],
     zip_safe=False,
     classifiers=[
         'License :: OSI Approved :: MIT License'
